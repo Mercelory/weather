2a41783aa83a9b9b90da63c85b9078f05ae71f7f
mercelory <mercelory@gmail.com>
2023-02-02 21:11:57 +0200

    styles added

--- src/App.js
+++ src/App.js
@@ -25,43 +25,43 @@ function App() {
   return (
     <ChakraProvider>
       <div className='h-screen py-5'>
-    <div  className='p-5 h-[100%] w-4/5 m-auto bg-slate-50/25 rounded-3xl flex justify-between items-center flex-col text-center' >
+    <div  className='p-5 h-[100%] w-4/5 m-auto bg-slate-900/25 rounded-3xl flex justify-between items-center flex-col text-center' >
       <div className='' id = "top">
  <InputGroup >
     <InputLeftElement 
       pointerEvents='none'
-      children={<SearchIcon color='black' width='15px' height='15px' />}/>
+      children={<SearchIcon color='rgb(208, 217, 224)' width='25px' height='25px' marginBottom='10px' />}/>
     <Input 
     type='text' 
     placeholder='Kyiv'
-    _placeholder={{ color: 'rgba(0,0,0,0.3)', fontWeight: 'bold' }}
+    _placeholder={{ color: 'rgb(208, 217, 224)', fontWeight: 'bold', fontSize: '30px',  padding: '15px', }}
     onKeyPress={searchLocation}
     onChange={event => setLocation(event.target.value)}
     value={location}
     borderWidth="0"
-    focusBorderColor='0'
+    focusBorderColor='rgb(208, 217, 224)'
     borderRadius='0'
     borderBottomWidth='2px'
-    borderColor='black'/>
+    borderColor='rgb(208, 217, 224)'/>
   </InputGroup>
   <div className='temp mt-24'>
-  {data.current ? <h1 className='text-4xl font-bold'>{data.current.temp_c} °C</h1> : null}
+  {data.current ? <h1 className='text-8xl font-bold'>{data.current.temp_c} °C</h1> : null}
   </div>
   <div className='time'>
-  {data.location ? <p className='text-xl mt-1'>{data.location.localtime}</p> : null}
+  {data.location ? <p className='text-3xl mt-1'>{data.location.localtime}</p> : null}
   </div>
   </div>
   {data.location!== undefined &&
   <div className = "flex flex-col justify-center items-center" id = "bottom">
     <div className='cloudness flex items-center'>
-  {data.current ? <img src = {data.current.condition.icon} alt = "weather_ico" className='w-6'/> : null }
-    {data.current.condition ? <p className='font-bold normal-case '>{data.current.condition.text}</p> : null}
+  {data.current ? <img src = {data.current.condition.icon} alt = "weather_ico" className='w-12'/> : null }
+    {data.current.condition ? <p className='font-bold normal-case text-3xl'>{data.current.condition.text}</p> : null}
     </div>
     <div className='Rainess flex'>
-    <p className='font-bold'>🌧️Rain - </p>
-    {data.current ? <p className='font-bold ml-1'>{data.current.humidity}%</p> : null}
+    <p className='font-bold text-3xl'>🌧️ Rain - </p>
+    {data.current ? <p className='font-bold ml-1 text-3xl'>{data.current.humidity}%</p> : null}
     </div>
-    <div className=' font-bold rounded-2xl p-5 mb-12 bg-slate-50/25 mt-2'>
+    <div className=' font-bold rounded-2xl p-5 mb-12 bg-slate-900/25 mt-2 text-3xl'>
     {data.location ? <h1>{data.location.name}, {data.location.country}</h1> : null}
     </div>
     {/* <div className='forecast bg-slate-50/25 h-36 overflow-x-scroll'>
