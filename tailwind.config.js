aaf55336045b8c8bd53a5656869afe19af32adc4
mercelory <mercelory@gmail.com>
2023-02-03 13:04:23 +0200

    heigh fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      'city-bg': "url('./src/bg-city.jpg')",
+      'bg': "url('./src/bg.jpg')",
+    },
+    screens: {
+      'sm': '300px',
+      // => @media (max-width: 300px) { ... }
+
+      'laptop': '1024px',
+      // => @media (max-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (max-width: 1280px) { ... }
     },
   },
   plugins: [],
