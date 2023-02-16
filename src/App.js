397b8c7dd75985d695745b524d1d6eb566093f36
Mercelory <mercelory@gmail.com>
2023-02-16 16:13:52 +0200

    design update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import { Input, InputLeftElement, InputGroup, ChakraProvider } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons'
+import { Input, InputLeftElement,InputRightElement, InputGroup, ChakraProvider,Button } from '@chakra-ui/react';
+import { TbMapPin,TbCloudRain,TbCloudFog } from "react-icons/tb";
 import React, {useState} from 'react'
 import axios from 'axios'
-import theme from './theme';
-import clsx from 'clsx';
+import SlideToggle from "react-slide-toggle";
 
 function App() {
 
@@ -15,10 +14,12 @@ function App() {
 
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };  
 
+  
   const searchLocation = (event) =>{
+    
     if(event.key === "Enter") {
       axios.get(url).then((response) => {
-        setData(response.data)
+        setData(response.data) 
       })
       setLocation('')
     }
@@ -26,77 +27,73 @@ function App() {
 
   return (
     <ChakraProvider>
-      <div>
-    <div className={clsx('p-5 w-screen m-auto bg-slate-900/25 flex justify-between items-center flex-col text-center',
-                        'md:h-screen')} >
-      <div className='' id = "top">
- <InputGroup >
-    <InputLeftElement 
+      <div className='h-screen w-screen flex justify-center items-center md:bg-white'>
+        <div className='md:h-4/5 md:w-2/4 bg-gradient-to-r from-blue-200 to-yellow-200 p-5 md:rounded-xl transition-all ease-in-out duration-1000 container flex flex-col h-screen w-screen overflow-auto'>
+        <InputGroup>
+    <InputLeftElement
       pointerEvents='none'
-      children={<SearchIcon color='rgb(208, 217, 224)' width='25px' height='25px' marginBottom='10px' />}/>
-    <Input 
-    type='text' 
-    placeholder='Kyiv'
-    _placeholder={{ color: 'rgb(208, 217, 224)', fontWeight: 'bold', fontSize: '30px',  padding: '15px', }}
-    onKeyPress={searchLocation}
-    onChange={event => setLocation(event.target.value)}
+      children={<TbMapPin className='w-6 h-6 text-slate-600'/>}
+    />
+    <Input placeholder='Search a city' type = "text" 
+onKeyPress={searchLocation}
+onChange={event => setLocation(event.target.value)}
     value={location}
-    borderWidth="0"
-    focusBorderColor='rgb(208, 217, 224)'
-    borderRadius='0'
-    borderBottomWidth='2px'
-    borderColor='rgb(208, 217, 224)'/>
+    border='none' className='justify-center items-center'/>
   </InputGroup>
-  <div className='temp mt-24'>
-  {data.current ? <h1 className={clsx('desktop:text-8xl font-bold',
-                                      'sm:text-5xl')}>{data.current.temp_c} °C</h1> : null}
+  {data.location!== undefined && <>
+  <div class="flex flex-col justify-center items-center transition-all ease-in-out duration-1000"> 
+    {data.location ? <p className='text-4xl font-bold mt-6'>{(new Date(data.location.localtime)).toLocaleDateString("en-US",{ month: 'short', day: '2-digit', })}</p> : null}
+    {data.current ? <img src={data.current.condition.icon} className='w-36' alt=''/> : null }
+    {data.current ? <p className='text-6xl font-bold'>{data.current.temp_c} °C</p> : null }
+    {data.current ? <p className=''>Feels like {data.current.feelslike_c} °C</p> : null }
+    {data.location ? <h1 className='text-xl mb-8'>{data.location.name}, {data.location.country}</h1> : null}
   </div>
-  <div className='time'>
-  {data.location ? <p className={clsx('desktop:text-3xl mt-1',
-                                      'sm:text-lg')}>{(new Date(data.location.localtime)).toLocaleDateString("en-US",{ month: 'short', day: '2-digit', })}</p> : null}
+  <div className='flex justify-between'>
+    <div className='flex items-center'>
+      <div>
+      <TbCloudFog className='w-8 h-8 mx-2'/>
+      </div>
+      <div>
+  {data.current.condition ? <p>{data.current.condition.text}</p> : null}
+  <p>Cloudness</p></div>
+  </div>
+  <div className='flex items-center'>
+    <div>
+    <TbCloudRain className='w-8 h-8 mx-2'/>
+    </div>
+    <div>
+  {data.current ? <p> {data.current.humidity}%</p> : null}
+  <p>Rain</p>
   </div>
   </div>
-  {data.location!== undefined &&
-  <div className = "flex flex-col justify-center items-center" id = "bottom">
-    <div className='cloudness flex items-center'>
-  {data.current ? <img src = {data.current.condition.icon} alt = "weather_ico" className={clsx('desktop:w-12', 'sm: w-8')}/> : null }
-    {data.current.condition ? <p className={clsx('font-bold normal-case desktop:text-3xl','sm:text-lg')}>{data.current.condition.text}</p> : null}
     </div>
-    <div className='Rainess flex'>
-    <p className={clsx('font-bold normal-case desktop:text-3xl','sm:text-lg')}>🌧️ Rain - </p>
-    {data.current ? <p className={clsx('font-bold normal-case desktop:text-3xl','sm:text-lg')}>{data.current.humidity}%</p> : null}
-    </div>
-    <div className={clsx('font-bold rounded-2xl p-5 mb-12 mt-2 desktop:text-3xl', 'sm:text-lg')}>
-    {data.location ? <h1>{data.location.name}, {data.location.country}</h1> : null}
-    </div>
-    <div className='forecast'>
-      <div className='forecast_days flex justify-evenly w-screen flex-wrap'>
-        <div className='forecast_day border-gray-200 border-2 rounded-2xl p-3 flex flex-col items-center mt-2'>
+    <div className=' flex overflow-scroll mt-8 snap-mandatory snap-x md:mt-0  md:overflow-visible h-fit'>
+      <div className='forecast_days flex  w-3/7 md:w-full h-fit'>
+        <div className='forecast_day border-gray-800/25 border-2 rounded-2xl p-3 flex flex-col items-center mt-2 mr-12 snap-center w-64 md:w-44'>
         {data.forecast.forecastday[1].day.condition ? <img src = {data.forecast.forecastday[1].day.condition.icon} alt = "weather_ico" className='w-12'/> : null }
-        {data.forecast.forecastday[1].day.condition ? <h1>{data.forecast.forecastday[1].day.condition.text}</h1> : null}
-        {data.forecast.forecastday[1].day.condition ? <h1>{data.forecast.forecastday[1].day.avgtemp_c}  °C</h1> : null}
+        {data.forecast.forecastday[1].day.condition ? <h1 className='text-center'>{data.forecast.forecastday[1].day.condition.text}</h1> : null}
+        {data.forecast.forecastday[1].day.condition ? <h1 className='text-center'>{data.forecast.forecastday[1].day.avgtemp_c}  °C</h1> : null}
         {data.forecast ? <h1>{(new Date(data.forecast.forecastday[1].date)).toLocaleDateString("en-US",{ month: 'short', day: '2-digit'})}</h1> : null}
         </div>
-        <div className='forecast_day border-gray-200 border-2 rounded-2xl p-3 flex flex-col items-center  mt-2'>
+        <div className='forecast_day border-gray-800/25 border-2 rounded-2xl p-3 flex flex-col items-center  mt-2 mr-12 snap-center w-64 md:w-44'>
         {data.forecast.forecastday[2].day.condition ? <img src = {data.forecast.forecastday[2].day.condition.icon} alt = "weather_ico" className='w-12'/> : null }
-        {data.forecast.forecastday[2].day.condition ? <h1>{data.forecast.forecastday[2].day.condition.text}</h1> : null}
-        {data.forecast.forecastday[2].day.condition ? <h1>{data.forecast.forecastday[2].day.avgtemp_c}  °C</h1> : null}
+        {data.forecast.forecastday[2].day.condition ? <h1 className='text-center'>{data.forecast.forecastday[2].day.condition.text}</h1> : null}
+        {data.forecast.forecastday[2].day.condition ? <h1 className='text-center'>{data.forecast.forecastday[2].day.avgtemp_c}  °C</h1> : null}
         {data.forecast ? <h1>{(new Date(data.forecast.forecastday[2].date)).toLocaleDateString("en-US",{ month: 'short', day: '2-digit'})}</h1> : null}
         </div>
-        <div className='forecast_day border-gray-200 border-2  rounded-2xl p-3 flex flex-col items-center  mt-2'>
+        <div className='forecast_day border-gray-800/25 border-2  rounded-2xl p-3 flex flex-col items-center  mt-2 snap-center w-64 md:w-44'>
         {data.forecast.forecastday[3].day.condition ? <img src = {data.forecast.forecastday[3].day.condition.icon} alt = "weather_ico" className='w-12'/> : null }
-        {data.forecast.forecastday[3].day.condition ? <h1>{data.forecast.forecastday[3].day.condition.text}</h1> : null}
-        {data.forecast.forecastday[3].day.condition ? <h1>{data.forecast.forecastday[3].day.avgtemp_c}  °C</h1> : null}
+        {data.forecast.forecastday[3].day.condition ? <h1 className='text-center'>{data.forecast.forecastday[3].day.condition.text}</h1> : null}
+        {data.forecast.forecastday[3].day.condition ? <h1 className='text-center'>{data.forecast.forecastday[3].day.avgtemp_c}  °C</h1> : null}
         {data.forecast ? <h1>{(new Date(data.forecast.forecastday[3].date)).toLocaleDateString("en-US",{ month: 'short', day: '2-digit'})}</h1> : null}
         </div>
-      </div>
-    </div> 
-  </div>
+    </div>
+    </div>
+    </>
 }
-    </div>
-    </div>
+</div>
+      </div>
     </ChakraProvider>
   );
 }
-
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'clouds': "url('/src/bg.jpg') !important",
-      }
-    },
-    screens: {
-      'sm': '300px',
-      // => @media (max-width: 300px) { ... }
-
-      'md': '500px',
-      // => @media (max-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (max-width: 1280px) { ... }
     },
   },
   plugins: [],
